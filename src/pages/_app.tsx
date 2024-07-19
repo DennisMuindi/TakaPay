82cbf6b11b30e761577e1bb810f2dfb4e97c38f1
DennisMuindi <mutukudennis7678@gmail.com>
2024-07-19 09:53:51 +0300

    new changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
 import { AppProps } from "next/app";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      require("bootstrap/dist/js/bootstrap.bundle.min.js");
-    }
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
